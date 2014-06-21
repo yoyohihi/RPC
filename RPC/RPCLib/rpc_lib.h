96be22e1eaff50e57657d253f758ba883057745e
hihiyoyo <jesse@ubuntu.(none)>
2014-06-20 20:49:39 -0700

    first commit

---
+++ RPC/RPCLib/rpc_lib.h
@@ -0,0 +1,52 @@
+#ifndef _RPC_LIB_H_
+#define _RPC_LIN_H_
+
+#include <stdint.h>
+
+
+namespace rpcLib
+{
+	namespace argTypes
+	{
+		typedef enum
+		{
+			ARG_UNKNOWN = 0,
+			ARG_CHAR  = 1,
+			ARG_SHORT = 2,
+			ARG_INT   = 3,
+			ARG_LONG  = 4,
+			ARG_DOUBLE= 5,
+			ARG_FLOAT = 6,
+			ARG_INPUT = 31,
+			ARG_OUTPUT= 30
+		} argType;
+
+		// I/O
+		bool isOutput(uint32_t type);
+		bool isInput(uint32_t type);
+
+        // Array
+		bool isArray(uint32_t type);
+		int getArraySize(uint32_t type);
+
+        // getter
+		argType getType(uint32_t type);
+
+        // create a new argType
+		int Create(bool isInput,bool isOutput, int type, int size = 0);
+	} // implementation in rpc_argTypes.cc
+
+
+	namespace returnCode
+	{
+		const int SUCCESS = 0;
+		const int FAIL    = -1;
+	}
+
+	namespace exception
+	{
+		class UnknownArgType{};
+	}
+}
+
+#endif
